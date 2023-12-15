14a33c7995061f4964d4b5ac56925d947fb88a7c
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-16 08:44:09 +1100

    dev env safeguard

--- src/app/as/page.tsx
+++ src/app/as/page.tsx
@@ -1,19 +1,26 @@
 import { IfLoggedIn } from "@/components/IfLoggedIn"
 import { JoinRoom, Register, GetRooms } from "./Forms"
+const { NODE_ENV } = process.env
 
 export default async function ApplicationServiceTest() {
+  console.log("NODE_ENV", NODE_ENV)
+
   return (
     <IfLoggedIn>
-      <h1>Application Service Test</h1>
+      {NODE_ENV === "development" && (
+        <>
+          <h1>Application Service Test</h1>
 
-      <h2>Get Rooms</h2>
-      <GetRooms />
+          <h2>Get Rooms</h2>
+          <GetRooms />
 
-      <h2>Join Room</h2>
-      <JoinRoom />
+          <h2>Join Room</h2>
+          <JoinRoom />
 
-      <h2>Register User</h2>
-      <Register />
+          <h2>Register User</h2>
+          <Register />
+        </>
+      )}
     </IfLoggedIn>
   )
 }
