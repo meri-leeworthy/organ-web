7f3c47f674329971127ba0f04e2f4350ca2affbd
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-17 11:08:20 +1100

    react dev tools script & heading style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import "@/styles/globals.css"
 import Username from "@/components/Username"
 import { Footer } from "@/components/Footer"
+import Script from "next/script"
 
 export const metadata = {
   title: "Organ",
@@ -18,10 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="w-screen flex justify-center min-h-screen">
+        {process.env.NODE_ENV === "development" && (
+          <Script src="http://localhost:8097" />
+        )}
         <div className="max-w-xl lg:max-w-3xl w-full p-2 lg:p-4">
           <header className="pb-4 mb-4 flex justify-between">
             <Link href="/">
-              <h1 className="text-lg lg:text-xl border border-black px-2 font-black leading-4">
+              <h1 className="text-xl border border-black py-1 px-2 font-black leading-4">
                 organ
               </h1>
             </Link>
