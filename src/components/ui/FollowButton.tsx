f7f4c859e42b1f35d2326d3c0f467af2be52fdfd
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-22 07:57:01 +1100

    follow button

---
+++ src/components/ui/FollowButton.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useClient } from "@/lib/useClient"
+import { IconCheck, IconPlus } from "@tabler/icons-react"
+import { useEffect, useState } from "react"
+
+export function FollowButton({ slug }: { slug: string }) {
+  const [isFollowing, setIsFollowing] = useState(false)
+
+  const client = useClient()
+
+  useEffect(() => {
+    if (!client) return
+    client
+      .getJoinedRooms()
+      .then(result => {
+        console.log("joined rooms result", result)
+        const isMember = result.joined_rooms.some(
+          roomId => roomId === `!${slug}:radical.directory`
+        )
+        setIsFollowing(isMember)
+      })
+      .catch(() => setIsFollowing(false))
+  }, [client, slug])
+
+  function handleJoinRoom() {
+    console.log("joining room")
+    if (!client) return
+    client
+      .joinRoom(`!${slug}:radical.directory`)
+      .then(() => {
+        console.log("joined room")
+        setIsFollowing(true)
+      })
+      .catch((err: any) => console.error("error joining room", err))
+  }
+
+  return (
+    <button
+      onClick={handleJoinRoom}
+      disabled={isFollowing}
+      className="flex items-center justify-center px-1 text-sm font-medium text-white border border-transparent bg-[#8258ff] disabled:border-[#8258ff] disabled:bg-white disabled:text-black disabled:opacity-60  rounded">
+      <span className="mr-1 text-xs uppercase">
+        {isFollowing ? "Following" : "Follow"}
+      </span>
+      {isFollowing ? <IconCheck size={16} /> : <IconPlus size={16} />}
+    </button>
+  )
+}
