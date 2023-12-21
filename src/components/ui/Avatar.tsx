08c9e1573f2898098635fbd6800ad3778b7c9c47
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-22 07:57:30 +1100

    create Avatar component

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,17 @@
+export function Avatar({
+  url,
+  name,
+}: {
+  url: string | undefined
+  name: string
+}) {
+  return (
+    <div className="flex items-center justify-center bg-primary rounded-full overflow-clip w-6 h-6">
+      {url ? (
+        <img className="" src={url} alt={name} />
+      ) : (
+        <span className="font-medium opacity-60">{name[0] || ""}</span>
+      )}
+    </div>
+  )
+}
