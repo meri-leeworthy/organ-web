3292b041cd92eb36617467af288ad60e8c2d4555
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-22 07:57:44 +1100

    create IfRoomMember component

---
+++ src/components/IfRoomMember.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useClient } from "@/lib/useClient"
+import { xor } from "@/lib/utils"
+import { useState, useEffect } from "react"
+
+export function IfRoomMember({
+  slug,
+  children,
+  not = false,
+}: {
+  slug: string
+  children: React.ReactNode
+  not?: boolean
+}) {
+  const [isMember, setIsMember] = useState(not)
+  const client = useClient()
+
+  useEffect(() => {
+    if (!client) return
+    client
+      .getJoinedRooms()
+      .then(result => {
+        // console.log("joined rooms result", result)
+        const isMember = result.joined_rooms.some(
+          roomId => roomId === `!${slug}:radical.directory`
+        )
+        setIsMember(isMember)
+      })
+      .catch(() => setIsMember(false))
+  }, [client, slug])
+
+  if (typeof window === "undefined") return
+  const accessToken = localStorage?.getItem("accessToken")
+  const userId = localStorage?.getItem("userId")
+
+  if (xor(isMember, not) && accessToken && userId) return <>{children}</>
+  return null
+}
