59f6c5bc2d8c69f76ceb07e902527b0111ed896a
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-22 07:58:35 +1100

    edit page refactor avatar

--- src/components/ui/UploadOrShowAvatar.tsx
+++ src/components/ui/UploadOrShowAvatar.tsx
@@ -2,27 +2,49 @@
 "use client"
 
 import { useClient } from "@/lib/useClient"
-import { IconPhotoFilled } from "@tabler/icons-react"
+import { IconEdit, IconPhoto } from "@tabler/icons-react"
 import { useRoom } from "@/lib/useRoom"
-import { getMxcUrl } from "@/lib/utils"
+import { getMessagesChunk, getMxcUrl } from "@/lib/utils"
+import { useEffect, useState } from "react"
 
 export function UploadOrShowAvatar({
   slug,
-  imageUri,
-}: {
+}: // imageUri,
+{
   slug: string
-  imageUri: string | null
+  // imageUri: string | null
 }) {
+  const [imageUri, setImageUri] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const room = useRoom(slug)
+  useEffect(() => {
+    if (!room) return
+    const messagesIterator = room.getMessagesAsyncGenerator()
+    getMessagesChunk(messagesIterator).then(messagesChunk => {
+      const avatar = messagesChunk.find(
+        (message: Event) => message.type === "m.room.avatar"
+      )
+      console.log("avatar", avatar)
+      setImageUri(avatar?.content?.url)
+      setIsLoading(false)
+    })
+  }, [room])
+
   console.log("imageUri", imageUri)
   if (imageUri) {
     const avatarUrl = getMxcUrl(imageUri)
-    return <img src={avatarUrl} alt="avatar" width="120" />
+    return (
+      <div className="flex items-end relative">
+        <img src={avatarUrl} alt="avatar" width="120" />
+        <UploadAvatar {...{ slug }} edit />
+      </div>
+    )
   }
 
   return <UploadAvatar {...{ slug }} />
 }
 
-function UploadAvatar({ slug }: { slug: string }) {
+function UploadAvatar({ slug, edit }: { slug: string; edit?: boolean }) {
   const client = useClient()
   const room = useRoom(slug)
   if (!client || !room) return <div>loading...</div>
@@ -38,19 +60,19 @@ function UploadAvatar({ slug }: { slug: string }) {
   }
 
   return (
-    <form className="flex">
+    <form className={`flex mt-2 ${edit ? "absolute top-2 left-2" : "w-72 "}`}>
       <label
         htmlFor="file"
-        className="flex gap-1 items-center bg-[#fff6] rounded-full px-2 self-start">
-        <IconPhotoFilled size={16} />
-        Add Profile Image
+        className="flex gap-1 text-xs items-center border border-transparent bg-[#ffffff99] hover:border-grey rounded uppercase px-1 self-start cursor-pointer">
+        {edit ? <IconEdit size={12} /> : <IconPhoto size={12} />}
+        {!edit && "Add Profile Image"}
       </label>
       <input
         type="file"
         id="file"
         onChange={handleFileChange}
         aria-label="Upload Avatar"
-        className="opacity-0 file:opacity-0"
+        className="opacity-0 w-0 h-0 file:opacity-0"
       />
     </form>
   )
