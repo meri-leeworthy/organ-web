6de718e189219c22487e87c5f7f0c66430f96bcd
Meri Leeworthy <mlee0167@student.monash.edu>
2023-12-29 09:19:01 +1100

    event & edit event page init

---
+++ src/app/id/[slug]/event/[id]/page.tsx
@@ -0,0 +1,54 @@
+const { AS_TOKEN, MATRIX_BASE_URL } = process.env
+
+// export const dynamic = "force-dynamic"
+
+import { Client, Room } from "simple-matrix-sdk"
+import Link from "next/link"
+import { getContextualDate } from "@/lib/utils"
+
+export default async function EventPage({
+  params,
+}: {
+  params: { slug: string; id: string }
+}) {
+  //TODO: this is a copy of PostPage needs changing (and refactor to share code?)
+
+  const { slug, id } = params
+  const roomId = `!${slug}:radical.directory`
+  const client = new Client(MATRIX_BASE_URL!, AS_TOKEN!, {
+    params: {
+      user_id: "@_relay_bot:radical.directory",
+    },
+    fetch,
+  })
+
+  const room = new Room(roomId, client)
+  const name = await room.getName()
+  const post = await room.getEvent(id)
+
+  console.log("room name", name)
+  console.log("post page", post)
+
+  const nameString =
+    typeof name === "object" &&
+    name !== null &&
+    "name" in name &&
+    typeof name.name === "string"
+      ? name.name
+      : ""
+
+  return (
+    <div className="w-full">
+      <Link
+        href={`/id/${slug}`}
+        className="bg-[#fff9] uppercase text-sm border rounded hover:border-primary px-2 py-1">
+        &larr; {nameString}
+      </Link>
+      <h1 className="py-4">{post.content?.title}</h1>
+      <span className="opacity-60 text-sm mt-8">
+        {getContextualDate(post.origin_server_ts)}
+      </span>
+      <p className="whitespace-pre-line py-2">{post.content?.body}</p>
+    </div>
+  )
+}
