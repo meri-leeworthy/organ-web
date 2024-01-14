76b456a2eab76f9ccdaf3c2c42be2fdcdcded534
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-15 10:29:21 +1100

    check username availability

--- src/app/user/new/page.tsx
+++ src/app/user/new/page.tsx
@@ -1,13 +1,29 @@
 "use client"
 
 import { Button, Input } from "@/components/styled"
-import React, { useState } from "react"
+import { useDebounce } from "@/hooks/useDebounce"
+import { IconCheck, IconX } from "@tabler/icons-react"
+import React, { useState, useEffect } from "react"
 import { Client } from "simple-matrix-sdk"
 
 const SignupForm = () => {
   const [email, setEmail] = useState("")
-  const [username, setUsername] = useState("")
+  const [debouncedUsername, username, setUsername] = useDebounce("", 500)
   const [password, setPassword] = useState("")
+  const [usernameAvailable, setUsernameAvailable] = useState(false)
+
+  useEffect(() => {
+    const checkUsername = async () => {
+      if (debouncedUsername.length > 0) {
+        const available = await Client.isUsernameAvailable(
+          debouncedUsername,
+          "https://matrix.radical.directory"
+        )
+        setUsernameAvailable(available)
+      }
+    }
+    checkUsername()
+  }, [debouncedUsername])
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value)
@@ -29,25 +45,56 @@ const SignupForm = () => {
         "https://matrix.radical.directory"
       )
       console.log(user, "User registered successfully!")
+
+      const { access_token: accessToken } = user
+
+      window.localStorage.setItem("accessToken", accessToken)
+      window.localStorage.setItem("userId", user.user_id)
+
+      const client = new Client(
+        "https://matrix.radical.directory",
+        accessToken,
+        {
+          userId: user.user_id,
+        }
+      )
+
+      // const 3pidResponse = client.add3pid()
     } catch (error) {
       console.error("Error registering user:", error)
     }
   }
 
   return (
-    <div className="w-full max-w-sm flex flex-col gap-2">
+    <div className="flex flex-col w-full max-w-sm gap-2">
       <h2 className="font-bold">New Account</h2>
       <form className="*:flex flex flex-col gap-2 *:flex-col">
         <label>
-          <span className="text-sm uppercase tracking-wider">Email</span>
+          <span className="text-sm tracking-wider uppercase">Email</span>
           <Input type="email" value={email} onChange={handleEmailChange} />
         </label>
         <label>
-          <span className="text-sm uppercase tracking-wider">Username</span>
+          <div className="flex items-baseline justify-between">
+            <span className="text-sm tracking-wider uppercase">Username</span>
+            <p className="text-xs uppercase opacity-80">
+              {debouncedUsername &&
+                (usernameAvailable ? (
+                  <div className="flex items-center gap-1 text-green-600">
+                    {"available"}
+                    <IconCheck size={12} />
+                  </div>
+                ) : (
+                  <div className="flex items-center gap-1 text-red-600">
+                    {"not available"}
+                    <IconX size={12} />
+                  </div>
+                ))}
+            </p>
+          </div>
           <Input type="text" value={username} onChange={handleUsernameChange} />
         </label>
         <label className="mb-4">
-          <span className="text-sm uppercase tracking-wider">Password</span>
+          <span className="text-sm tracking-wider uppercase">Password</span>
           <Input
             type="password"
             value={password}
@@ -57,7 +104,7 @@ const SignupForm = () => {
         <Button
           type="button"
           onClick={handleSignup}
-          className="self-start border-black border">
+          className="self-start border border-black hover:bg-primary hover:border-transparent">
           Sign Up
         </Button>
       </form>
