c1643a5194b3e55fc2636b58b166e3b15011b632
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-18 12:35:43 +1100

    memoise room

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,8 +1,25 @@
-import { Room } from "simple-matrix-sdk"
-import { useClient } from "./useClient"
+import { Client, Room } from "simple-matrix-sdk"
+import { useEffect, useMemo, useState } from "react"
+
+export const BASE_URL = "https://matrix.radical.directory"
+export const ACCESSTOKEN_STORAGE_KEY = "accessToken"
+export const USERID_STORAGE_KEY = "userId"
 
 export function useRoom(slug: string) {
-  const client = useClient()
-  if (!client) return undefined
-  return new Room(`!${slug}:radical.directory`, client)
+  const [room, setRoom] = useState<Room | null>(null)
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem(ACCESSTOKEN_STORAGE_KEY)
+    const userId = localStorage.getItem(USERID_STORAGE_KEY)
+
+    if (accessToken && userId) {
+      const client = new Client(BASE_URL, accessToken, { userId, fetch })
+      const room = new Room(`!${slug}:radical.directory`, client)
+      setRoom(room)
+    }
+  }, [slug])
+
+  const memoizedRoom = useMemo(() => room, [room])
+
+  return memoizedRoom
 }
