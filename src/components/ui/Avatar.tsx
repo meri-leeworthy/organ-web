712e522891aa872cd466b0c19a83f1adde510f5a
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-19 09:39:31 +1100

    stay round

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -8,11 +8,11 @@ export function Avatar({
   name: string
 }) {
   return (
-    <div className="flex items-center justify-center w-6 h-6 rounded-full bg-primary overflow-clip">
+    <div className="flex items-center justify-center h-6 rounded-full shrink-0 aspect-square bg-primary overflow-clip">
       {url ? (
         <img className="" src={getMxcUrl(url)} alt={name} />
       ) : (
-        <span className="font-medium text-primarydark">
+        <span className="w-full font-medium text-center text-primarydark">
           {(name && name[0]) || ""}
         </span>
       )}
