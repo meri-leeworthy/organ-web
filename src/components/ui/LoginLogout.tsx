8d827c9b498e856c0b23fd340bdba681b3b97514
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-29 10:31:06 +1100

    refactor and remove redundant code

--- src/components/ui/LoginLogout.tsx
+++ src/components/ui/LoginLogout.tsx
@@ -4,34 +4,37 @@ import { useRouter } from "next/navigation"
 import { Suspense } from "react"
 import { Button } from "../styled"
 import { ACCESSTOKEN_STORAGE_KEY, USERID_STORAGE_KEY } from "@/lib/constants"
+import { IfLoggedIn } from "../IfLoggedIn"
 
 const LoginLogout = () => {
+  return (
+    <Suspense fallback={<Login />}>
+      <IfLoggedIn fallback={<Login />}>
+        <Logout />
+      </IfLoggedIn>
+    </Suspense>
+  )
+}
+
+const Login = () => {
   const router = useRouter()
-  const accessToken =
-    typeof localStorage !== "undefined" &&
-    localStorage.getItem(ACCESSTOKEN_STORAGE_KEY)
-  const userId =
-    typeof localStorage !== "undefined" &&
-    localStorage.getItem(USERID_STORAGE_KEY)
 
-  const loggedIn = accessToken && userId && true
-
-  const handleLogout = () => {
-    if (typeof window === "undefined") return null
-    if (loggedIn) {
-      localStorage.removeItem(ACCESSTOKEN_STORAGE_KEY)
-      localStorage.removeItem(USERID_STORAGE_KEY)
-      location.reload()
-    } else {
-      router.push("/login")
-    }
+  const handleLogin = () => {
+    router.push("/login")
   }
 
-  return (
-    <Button onClick={handleLogout}>
-      <Suspense fallback="login">{loggedIn ? "logout" : "login"}</Suspense>
-    </Button>
-  )
+  return <Button onClick={handleLogin}>login</Button>
+}
+
+const Logout = () => {
+  const handleLogout = () => {
+    if (typeof window === "undefined") return null
+    localStorage.removeItem(ACCESSTOKEN_STORAGE_KEY)
+    localStorage.removeItem(USERID_STORAGE_KEY)
+    location.reload()
+  }
+
+  return <Button onClick={handleLogout}>logout</Button>
 }
 
 export default LoginLogout
