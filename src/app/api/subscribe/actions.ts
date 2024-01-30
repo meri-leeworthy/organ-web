7da6df27d29643d9b36401da44c59f1d8937523a
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-30 16:01:55 +1100

    subscribe email to room

---
+++ src/app/api/subscribe/actions.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import { organRoomUserNotifications } from "@/lib/types"
+import { Client, Room } from "simple-matrix-sdk"
+
+const { MATRIX_BASE_URL, AS_TOKEN } = process.env
+
+export async function subscribeEmailToRoom(roomId: string, hash: string) {
+  const client = new Client(MATRIX_BASE_URL!, AS_TOKEN!, {
+    fetch,
+    params: {
+      user_id: "@_relay_bot:radical.directory",
+    },
+  })
+  const room = new Room(roomId, client)
+
+  const powerLevels = await room.getPowerLevels()
+  console.log("powerLevels", powerLevels)
+
+  const res = await room.sendStateEvent(
+    organRoomUserNotifications,
+    {
+      email: "every",
+    },
+    hash
+  )
+  console.log("state sent", res)
+
+  return res
+}
