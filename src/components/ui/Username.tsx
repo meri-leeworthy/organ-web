6a7f48ade59b9b834606747b77f6ba0a57c9daa6
Meri Leeworthy <mlee0167@student.monash.edu>
2024-02-01 09:39:22 +1100

    show Login button in header

--- src/components/ui/Username.tsx
+++ src/components/ui/Username.tsx
@@ -2,6 +2,7 @@
 
 import { useClient } from "@/hooks/useClient"
 import React, { useEffect, useState } from "react"
+import LoginLogout from "./LoginLogout"
 
 export const Username: React.FC = () => {
   const [username, setUsername] = useState<string | null>(null)
@@ -13,6 +14,8 @@ export const Username: React.FC = () => {
       setUsername(profile.displayname)
     })
   }, [client])
+
+  if (!username) return <LoginLogout />
 
   return <div className="text-sm">{username}</div>
 }
