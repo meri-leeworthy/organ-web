00d6827b65948b48c17006376bd7ff8a02176dc2
Meri Leeworthy <mlee0167@student.monash.edu>
2024-01-31 14:05:46 +1100

    check notification settings; html email; error check

--- src/app/api/email-dispatch/route.ts
+++ src/app/api/email-dispatch/route.ts
@@ -1,5 +1,6 @@
 const { MATRIX_BASE_URL, AS_TOKEN } = process.env
 import { getSecretFromRoom } from "@/lib/roomSecretStore"
+import { sendEmail } from "@/lib/sendEmail"
 import {
   OrganCalEventUnstable,
   OrganPostUnstable,
@@ -33,7 +34,10 @@ export async function POST(request: NextRequest) {
 
   const hashes = state
     .filter((event: any) => {
-      return event.type === organRoomUserNotifications
+      return (
+        event.type === organRoomUserNotifications &&
+        event.content.email === "every"
+      )
     })
     .map((event: any) => {
       return event.state_key
@@ -47,8 +51,15 @@ export async function POST(request: NextRequest) {
     )
     console.log("roomId", roomId)
     const email = await getSecretFromRoom(roomId, organRoomSecretEmail)
+    if ("errcode" in email) return
     console.log("email", email)
     const room = new Room(roomId, client)
+
+    await sendEmail(
+      email.body,
+      "New post on Organ!", //would be good to include the name of the room
+      "<h1>" + post.title + "</h1>\n" + post.body // include some descriptive things & links, html formatting?
+    )
   })
 
   return NextResponse.json({
