42e7bef651a53758909c169c875901ae272ff5c9
Meri Leeworthy <mlee0167@student.monash.edu>
2024-02-01 09:42:44 +1100

    twMerge classes

--- src/components/styled/Button.tsx
+++ src/components/styled/Button.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge"
+
 export function Button({
   children,
   className,
@@ -6,9 +8,13 @@ export function Button({
   children: React.ReactNode
   className?: string
 } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  const classes =
-    `rounded-[100%] px-2 py-1 ${className} ` +
-    (className === undefined && "border border-black hover:border-dashed")
+  const classes = twMerge(
+    "rounded-[100%] px-2 py-1",
+    "border border-black hover:border-dashed",
+    className
+  )
+  // `rounded-[100%] px-2 py-1 ${className} ` +
+  // (className === undefined && "border border-black hover:border-dashed")
   return (
     <button className={classes} {...props}>
       {children}
