95bc81907ddd380d761db7f65ac68ae4d22e95df
Meri Leeworthy <mlee0167@student.monash.edu>
2024-02-19 16:20:37 +1100

    store email secret; correct nameString use

--- src/app/api/subscribe/actions.ts
+++ src/app/api/subscribe/actions.ts
@@ -1,14 +1,22 @@
 "use server"
 
 import { getHmac32 } from "@/lib/getHmac"
+import { storeSecretInRoom } from "@/lib/roomSecretStore"
 import { getOrCreateMailboxId, sendEmailFromMailbox } from "@/lib/sendEmail"
-import { organRoomUserNotifications } from "@/lib/types"
+import { organRoomSecretEmail, organRoomUserNotifications } from "@/lib/types"
 import { Client, Room } from "simple-matrix-sdk"
 
 const { MATRIX_BASE_URL, AS_TOKEN } = process.env
 
 export async function subscribeEmailToRoom(roomId: string, email: string) {
-  await getOrCreateMailboxId(email)
+  const mailboxRoomId = await getOrCreateMailboxId(email)
+  const stored = await storeSecretInRoom(
+    mailboxRoomId as string,
+    organRoomSecretEmail,
+    email,
+  )
+  console.log("stored", stored)
+
   const hash = getHmac32(email)
 
   const client = new Client(MATRIX_BASE_URL!, AS_TOKEN!, {
@@ -17,11 +25,13 @@ export async function subscribeEmailToRoom(roomId: string, email: string) {
       user_id: "@_relay_bot:radical.directory",
     },
   })
+
   const room = new Room(roomId, client)
 
   const name = await room.getName()
   if (typeof name === "object" && name !== null && "errcode" in name)
     return name
+
   console.log("name", name)
 
   const nameString =
@@ -30,7 +40,7 @@ export async function subscribeEmailToRoom(roomId: string, email: string) {
       : "unnamed room"
 
   const pageSpecificWelcomeEmailContent = `
-  Hi, confirming you just for signed up to get updates about ${nameString} on Organ!
+  Hi, confirming you just signed up to get updates about ${nameString} on Organ!
 `
 
   const res = await room.sendStateEvent(
@@ -50,7 +60,8 @@ export async function subscribeEmailToRoom(roomId: string, email: string) {
   const result = await sendEmailFromMailbox(
     `relay_${hash}`,
     email as string,
-    `You just signed up for updates for ${name} on Organ!`,
+    `You just signed up for updates for ${nameString} on Organ!`,
+    pageSpecificWelcomeEmailContent,
     pageSpecificWelcomeEmailContent,
   )
 
