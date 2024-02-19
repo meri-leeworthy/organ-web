271fdcbdf9e95516df3c13f7572efc2b69800c89
Meri Leeworthy <mlee0167@student.monash.edu>
2024-02-19 16:23:03 +1100

    new data model types

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -49,19 +49,80 @@ export type OrganMetaContactUnstable = {
 }
 
 export const organMetaContactUnstable = "organ.meta.contact.unstable"
-
 export const organPostUnstable = "organ.post.unstable"
-
 export const organRoomUserNotifications = "organ.room.user.notifications"
-
 export const organRoomType = "organ.room.type"
-
 export const organRoomSecretEmail = "organ.room.secret.email"
+export const organSpaceType = "organ.space.type"
+export const organLocation = "organ.location"
+export const organEventMeta = "organ.event.meta"
 
 export const organRoomTypeValue = {
   id: "id",
   event: "event",
+  post: "post",
 } as const
+
+export type OrganLocation =
+  | {
+      type: "text"
+      value: string
+    }
+  | {
+      type: "page"
+      value: string
+    }
+
+export type OrganEventMeta = {
+  type: typeof organEventMeta
+  content: {
+    start: string
+    end: string
+    allDay?: boolean
+    location?: OrganLocation
+    url?: string
+  }
+}
+
+export const organSpaceTypeValue = {
+  tagindex: "tagindex",
+  userindex: "userindex",
+  tag: "tag",
+  page: "page",
+  event: "event",
+} as const
+
+// SPACE/ROOM STATE MODEL
+//
+// POST
+// organ.room.type = "post"
+// event?: string //parent
+// pages?: string[] //parents
+// tags?: string[] //parents
+// content: {type: "text", body: string} | {type: "private", id: Event ID}  //content
+//
+// EVENT
+// organ.space.type = "event"
+// events?: string[] //linked events (roomID)
+// pages?: string[] //parents (roomID)
+// tags?: string[] //parents (roomID)
+//
+//
+// PAGE
+// organ.space.type = "page"
+// pages?: string[] //linked pages (roomID)
+// tags?: string[] //parents (roomID)
+// pinned: string[] //pinned posts or events (roomID)
+//
+// TAG
+// organ.space.type = "tag"
+// pinned?: string[] //pinned pages, events or posts (children) (roomID)
+// canonical?: string //page (child) (roomID)
+// pages: Map<RoomID, PageName> // children (essentially a cached AS aggregation)
+//
+// TAGINDEX
+// organ.space.type = "tagindex"
+// tags: Map<RoomID, Tag> // children (essentially a cached AS aggregation)
 
 export const OrganPostUnstableSchema = object({
   title: string("title must be a string"),
