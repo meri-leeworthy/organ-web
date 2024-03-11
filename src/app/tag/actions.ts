a811169c8818f7654b16697be0bb4a20ea6cd67d
Meri Leeworthy <mlee0167@student.monash.edu>
2024-03-11 20:57:40 +1100

    move tag functions to actions

--- src/app/tag/actions.ts
+++ src/app/tag/actions.ts
@@ -2,10 +2,10 @@
 
 const { MATRIX_BASE_URL, AS_TOKEN, TAG_INDEX } = process.env
 
-import { bilateralAdoptTag } from "@/lib/tag"
 import { organSpaceType, organSpaceTypeValue } from "@/lib/types"
 import { normaliseTagString } from "@/lib/utils"
-import { Client, Room } from "simple-matrix-sdk"
+import { Client, ErrorSchema, Room } from "simple-matrix-sdk"
+import { is } from "valibot"
 
 const client = new Client(MATRIX_BASE_URL!, AS_TOKEN!, {
   fetch,
@@ -13,6 +13,55 @@ const client = new Client(MATRIX_BASE_URL!, AS_TOKEN!, {
 })
 
 const index = new Room(TAG_INDEX!, client)
+
+export async function createTag(opts: {
+  name: string
+  description: string
+  // pinned?: string[] //pinned pages, events or posts (children)
+  // canonical?: string //page (child)
+  slug?: string
+}) {
+  const tagSpace = await client.createRoom({
+    creation_content: { type: "m.space" },
+    name: opts.name,
+    topic: opts.description,
+    room_alias_name: opts.slug,
+    // visibility: "public",
+    initial_state: [
+      {
+        type: "m.room.join_rules",
+        content: {
+          join_rule: "public"
+        }
+      },
+      {
+        type: organSpaceType,
+        content: {
+          type: organSpaceTypeValue.tag
+        }
+      },
+      {
+        type: "m.space.parent",
+        state_key: TAG_INDEX,
+        content: {
+          via: ["radical.directory"]
+        }
+      }
+    ]
+  })
+
+  if (is(ErrorSchema, tagSpace)) return tagSpace
+
+  await tagIndex.sendStateEvent(
+    "m.space.child",
+    {
+      via: ["radical.directory"]
+    },
+    tagSpace.roomId
+  )
+
+  return tagSpace
+}
 
 export async function addTag(formData: FormData) {
   const unNormalisedTag = (formData.get("tag") as string) || ""
@@ -50,4 +99,80 @@ export async function addTag(formData: FormData) {
 
 export async function removeTag(tagEventId: string) {
   return await index.redactEvent(tagEventId)
+}
+
+const tagIndex = new Room(TAG_INDEX!, client)
+
+export async function bilateralAdoptTag(tagRoomId: string) {
+  const tagRoom = new Room(tagRoomId, client)
+  const tagName = await tagRoom.getName()
+
+  const tagNameString =
+    typeof tagName === "object" &&
+    tagName !== null &&
+    "name" in tagName &&
+    tagName.name
+
+  // add tag as child to tag index
+  const tagIndexAddChild = await tagIndex.sendStateEvent(
+    "m.space.child",
+    {
+      order: tagNameString,
+      via: ["radical.directory"]
+    },
+    tagRoomId
+  )
+
+  // add tag index as parent to tag
+  const tagAddParent = await tagRoom.sendStateEvent(
+    "m.space.parent",
+    {
+      via: ["radical.directory"]
+    },
+    TAG_INDEX
+  )
+
+  return { tagIndexAddChild, tagAddParent }
+}
+
+export async function bilateralTagAdoptPost(
+  postRoomId: string,
+  tagRoomId: string
+) {
+  const tagRoom = new Room(tagRoomId, client)
+
+  const postRoom = new Room(postRoomId, client)
+
+  // add post as child to tag space
+  const tagRoomAddChild = await tagRoom.sendStateEvent(
+    "m.space.child",
+    {
+      via: ["radical.directory"]
+    },
+    postRoomId
+  )
+
+  // add tag space as parent to post
+  const postAddParent = await postRoom.sendStateEvent(
+    "m.space.parent",
+    {
+      via: ["radical.directory"]
+    },
+    tagRoomId
+  )
+
+  return { tagRoomAddChild, postAddParent }
+}
+
+export async function searchTags(tag: string) {
+  const tagRoomId = await client.getRoomIdFromAlias(
+    `#relay_tag_${normaliseTagString(tag)}:radical.directory`
+  )
+  if (typeof tagRoomId === "object" && "errcode" in tagRoomId) return tagRoomId
+  const tagRoom = new Room(tagRoomId, client)
+  const tagState = await tagRoom.getState()
+  if ("errcode" in tagState) return tagState
+  const children = tagState.getAll("m.space.child")
+  // console.log(tag, "children", children)
+  return children
 }
--- src/lib/create.ts
+++ src/lib/create.ts
@@ -1,18 +1,15 @@
 "use server"
 
-import { Client, Room, ErrorSchema } from "simple-matrix-sdk"
+import { Client, Room } from "simple-matrix-sdk"
 import {
   OrganPageEventMeta,
   OrganLocation,
   organLocation,
-  organRoomType,
-  organRoomTypeValue,
   organSpaceType,
   organSpaceTypeValue,
   organPageType,
   organPageTypeValue
 } from "./types"
-import { is } from "valibot"
 
 const { MATRIX_BASE_URL, AS_TOKEN, TAG_INDEX } = process.env
 
@@ -20,49 +17,6 @@ const client = new Client(MATRIX_BASE_URL!, AS_TOKEN!, {
   fetch,
   params: { user_id: "@_relay_bot:radical.directory" }
 })
-
-const tagIndex = new Room(TAG_INDEX!, client)
-
-export async function createPost(opts: {
-  owner: string
-  title?: string
-  body: string
-  // event?: string //parent
-  // pages?: string[] //parents
-  // tags?: string[] //parents
-}) {
-  const postRoom = await client.createRoom({
-    name: opts.title,
-    topic: opts.body,
-    // visibility: "public",
-    invite: [opts.owner],
-    initial_state: [
-      {
-        type: "m.room.join_rules",
-        content: {
-          join_rule: "public"
-        }
-      },
-      {
-        type: organRoomType,
-        content: {
-          type: organRoomTypeValue.post
-        }
-      }
-    ]
-  })
-
-  return postRoom
-  // set organ.room.type = post
-  // create a page as relay bot
-  // invite user creator as admin
-  // set name to title
-  // set topic to body
-  // set preset to public world readable
-  // set history visibility to shared
-  //
-  // adding media comes later (not in this function)
-}
 
 export async function createEvent(opts: {
   owner: string
@@ -148,53 +102,4 @@ export async function createPage(opts: {
     initial_state: initial_state
   })
   return pageSpace
-}
-
-export async function createTag(opts: {
-  name: string
-  description: string
-  // pinned?: string[] //pinned pages, events or posts (children)
-  // canonical?: string //page (child)
-  slug?: string
-}) {
-  const tagSpace = await client.createRoom({
-    creation_content: { type: "m.space" },
-    name: opts.name,
-    topic: opts.description,
-    room_alias_name: opts.slug,
-    // visibility: "public",
-    initial_state: [
-      {
-        type: "m.room.join_rules",
-        content: {
-          join_rule: "public"
-        }
-      },
-      {
-        type: organSpaceType,
-        content: {
-          type: organSpaceTypeValue.tag
-        }
-      },
-      {
-        type: "m.space.parent",
-        state_key: TAG_INDEX,
-        content: {
-          via: ["radical.directory"]
-        }
-      }
-    ]
-  })
-
-  if (is(ErrorSchema, tagSpace)) return tagSpace
-
-  await tagIndex.sendStateEvent(
-    "m.space.child",
-    {
-      via: ["radical.directory"]
-    },
-    tagSpace.roomId
-  )
-
-  return tagSpace
 }
