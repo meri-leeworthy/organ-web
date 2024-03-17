8e7e241edf32126955b26e9f54c566400929cd65
Meri Leeworthy <mlee0167@student.monash.edu>
2024-03-17 22:07:45 +1100

    type guards, fix import

--- src/app/tag/[tag]/Item.tsx
+++ src/app/tag/[tag]/Item.tsx
@@ -1,4 +1,5 @@
 import { client } from "@/lib/client"
+import { props } from "@/lib/utils"
 import { Room } from "simple-matrix-sdk"
 
 export async function Item({ postRoomId }: { postRoomId: string }) {
@@ -6,16 +7,30 @@ export async function Item({ postRoomId }: { postRoomId: string }) {
   const state = await postRoom.getState()
   console.log("state", state)
   if ("errcode" in state) return "no state"
-  const roomType = state.get("organ.room.type")
-  if (!roomType || roomType.content.type !== "post") return "not a post room"
-  const postType = state.get("organ.post.type")
-  if (!postType) return "not a post room"
-  if (postType.content.type !== "text") return "not a text post"
+  const roomTypeEvent = state.get("organ.room.type")
+  if (!roomTypeEvent) return "not a post room"
+  const { content: roomType } = roomTypeEvent
+  if (
+    !roomType ||
+    typeof roomType !== "object" ||
+    !("type" in roomType) ||
+    !("value" in roomType)
+  )
+    return "not a post room"
+  if (roomType?.type !== "post") return "not a post room"
+
+  const postTypeEvent = state.get("organ.post.type")
+  if (!postTypeEvent) return "not a post room"
+  const { content: postType } = postTypeEvent
+  if (!postType || typeof postType !== "object" || !("type" in postType))
+    return "not a post room"
+  if (postType.type !== "text") return "not a text post"
   const postText = state.get("organ.post.text")
+  const text = props(postText, "content", "value")
   console.log("roomType", roomType)
   return (
     <li>
-      <p>{postText.content.value}</p>
+      <p>{typeof text === "string" && text}</p>
       {/* <h1>{postRoom.name}</h1>
       <p>{postRoom.description}</p> */}
     </li>
--- src/app/tag/[tag]/page.tsx
+++ src/app/tag/[tag]/page.tsx
@@ -1,4 +1,4 @@
-import { searchTags } from "@/lib/tag"
+import { searchTags } from "@/app/tag/actions"
 import { Item } from "./Item"
 
 export default async function TagPage({ params }: { params: { tag: string } }) {
@@ -14,7 +14,7 @@ export default async function TagPage({ params }: { params: { tag: string } }) {
     <div>
       <h1>Pages & posts tagged with {tag}</h1>
       <ul>
-        {[...searchTagsResult.keys()].map((key) => (
+        {[...searchTagsResult.keys()].map(key => (
           <Item key={key} postRoomId={key} />
         ))}
       </ul>
