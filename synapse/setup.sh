4ebd2464a1ebf08f71738dfbcd9fe0dea2ff8977
Meri Leeworthy <mlee0167@student.monash.edu>
2024-03-17 22:06:05 +1100

    talkative shell script

--- synapse/setup.sh
+++ synapse/setup.sh
@@ -1,4 +1,8 @@
+echo "Setting permissions on /data"
 chown -R 991:991 /data
+echo "Putting relay.yml in /data/appservices"
 mkdir /data/appservices
 cp /as/relay.yml /data/appservices/relay.yml
+echo "Configuring homeserver.yaml to use relay.yml"
 echo -e "\napp_service_config_files:\n  - /data/appservices/relay.yml" >> data/homeserver.yaml 
+echo "Done setting up appservices."
