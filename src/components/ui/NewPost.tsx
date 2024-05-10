ade7ee5f312622cb62280d64c8ba8448eff9b30c
Meri Leeworthy <mlee0167@student.monash.edu>
2024-05-10 12:29:42 +1000

    timepicker etc

--- src/components/ui/NewPost.tsx
+++ src/components/ui/NewPost.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
 
-import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react"
+import {
+  Dispatch,
+  RefObject,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react"
 import { useClient } from "@/hooks/useClient"
 import {
   // OrganCalEventUnstable,
@@ -12,20 +20,35 @@ import {
   organRoomType,
   organRoomTypeTree,
   organRoomTypeValue,
+  organSpaceType,
+  organSpaceTypeValue,
 } from "@/types/schema"
 
+import { TimePicker as AntDTimePicker, ConfigProvider } from "antd"
 import { Room } from "simple-matrix-sdk"
 import {
   IconCalendarEvent,
-  IconCamera,
+  IconClock,
   IconNorthStar,
+  IconX,
 } from "@tabler/icons-react"
 // import { SelectAuthor } from "@/components/SelectAuthor"
 // import Link from "next/link"
-import { Spinner } from "./Spinner"
-import { getMxcUrl, toValidDateString } from "@/lib/utils"
+import { toValidDateString } from "@/lib/utils"
 import { Input, Textarea, Button } from "../styled"
-import { organPostMeta } from "@/types/post"
+import { Input as SCNInput } from "./input"
+import { OrganPostMetaState, organPostMeta } from "@/types/post"
+import { organCalEventMeta } from "@/types/event"
+import { UploadImageButton } from "./UploadImageButton"
+import { DatePicker } from "./DatePicker"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select"
+import { StateEvent } from "@/types/utils"
 
 type PostType = "post" | "event"
 
@@ -33,14 +56,16 @@ export const NewPost = ({ slug }: { slug: string }) => {
   const [type, setType] = useState<PostType>("post")
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
-  const [startDate, setStartDate] = useState(toValidDateString(new Date()))
-  const [startTime, setStartTime] = useState("00:00")
+  const [startDate, setStartDate] = useState<Date>()
+  // const [startTime, setStartTime] = useState("00:00")
   const [endDate, setEndDate] = useState(toValidDateString(new Date()))
   const [endTime, setEndTime] = useState("00:00")
   const [place, setPlace] = useState("")
   const [imageSrcs, setImageSrcs] = useState<string[]>([])
   const [author, setAuthor] = useState<[Room, string]>()
   const [allDay, setAllDay] = useState(false)
+
+  // const [hours, minutes] = startTime.split(":").map(Number)
 
   // console.log("imageSrcs", imageSrcs)
 
@@ -69,47 +94,62 @@ export const NewPost = ({ slug }: { slug: string }) => {
   // console.log("datetime", startTime, startDate)
 
   async function handlePostSubmit(event: React.FormEvent<HTMLFormElement>) {
-    // const authorRoom = new Room(content?.author, client)
-    // const roomName = await authorRoom.getName()
-    // console.log("roomName", roomName)
-    // const roomNameString =
-    //   roomName &&
-    //   typeof roomName === "object" &&
-    //   roomName !== null &&
-    //   "name" in roomName &&
-    //   typeof roomName.name === "string"
-    //     ? roomName.name
-    //     : ""
-
     event.preventDefault()
 
     if (!author) return
     const [authorRoom, authorName] = author
     const roomId = authorRoom.roomId
-    // const authorKV = { name: authorName, id: roomId }
 
-    // const postEvent: OrganPostUnstable | OrganCalEventUnstable =
-    //   type === "post"
-    //     ? {
-    //         msgtype: organPostUnstable,
-    //         title,
-    //         body: content,
-    //         author: authorKV,
-    //         tags: [],
-    //         media: imageSrcs
-    //       }
-    //     : {
-    //         msgtype: organCalEventUnstable,
-    //         title,
-    //         body: content,
-    //         host: authorKV,
-    //         tags: [],
-    //         start: `${startDate}T${startTime}`,
-    //         end: `${endDate}T${endTime}`,
-    //         allDay,
-    //         avatar: imageSrcs[0],
-    //         location: place
-    //       }
+    const postMetaState: (
+      | OrganPostMetaState
+      | StateEvent<string, { value: string }>
+    )[] = [
+      {
+        type: organRoomType,
+        content: {
+          value: organRoomTypeValue.post,
+        },
+      },
+      {
+        type: organPostType,
+        content: {
+          value: organRoomTypeTree.post.text,
+        },
+      },
+      {
+        type: organPostMeta,
+        content: {
+          title,
+          body: content,
+          timestamp: new Date().valueOf(),
+          author: {
+            type: "id",
+            value: `!${slug}:${process.env.NEXT_PUBLIC_SERVER_NAME}`,
+          },
+        },
+      },
+    ]
+
+    const eventMetaState = [
+      {
+        type: organSpaceType,
+        content: {
+          value: organSpaceTypeValue.event,
+        },
+      },
+      {
+        type: organCalEventMeta,
+        content: {
+          start: startDate,
+          end: endDate,
+          location,
+          allDay,
+        },
+      },
+    ]
+
+    const postOrEventMetaState =
+      type === "post" ? postMetaState : eventMetaState
 
     const newPostRoomResult = await client?.createRoom({
       name: title,
@@ -143,51 +183,13 @@ export const NewPost = ({ slug }: { slug: string }) => {
           },
         },
         {
-          type: organRoomType,
-          state_key: "",
-          content: {
-            type: organRoomTypeValue.post,
-          },
-        },
-        {
-          type: organPostType,
-          state_key: "",
-          content: {
-            type: organRoomTypeTree.post.text,
-          },
-        },
-        {
-          type: organPostMeta,
-          content: {
-            title,
-            content,
-            timestamp: new Date().valueOf(),
-            author: {
-              type: "id",
-              value: `!${slug}:${process.env.NEXT_PUBLIC_SERVER_NAME}`,
-            },
-          },
-        },
-        {
-          type: organPostMeta,
-          state_key: "",
-          content: {
-            title,
-            content,
-            author: {
-              type: "id",
-              value: `!${slug}:${process.env.NEXT_PUBLIC_SERVER_NAME}`,
-            },
-            timestamp: new Date().valueOf(),
-          },
-        },
-        {
           type: "m.space.parent",
           state_key: `!${slug}:${process.env.NEXT_PUBLIC_SERVER_NAME}`,
           content: {
             via: [process.env.NEXT_PUBLIC_SERVER_NAME],
           },
         },
+        ...postOrEventMetaState,
       ],
       invite: ["@_relay_bot:" + process.env.NEXT_PUBLIC_SERVER_NAME],
     })
@@ -206,10 +208,27 @@ export const NewPost = ({ slug }: { slug: string }) => {
 
     console.log("adoptionResult", adoptionResult)
 
+    const join = await fetch(`/api/join?roomId=${newPostRoomResult.roomId}`)
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.error)
+    console.log("join", join)
+
     // const result = await room?.sendMessage(postEvent)
 
+    // send to postsBus via api call
+    const postsBusId = await fetch("/api/bus", {
+      method: "POST",
+      body: JSON.stringify({
+        type: "post",
+        value: newPostRoomResult.roomId,
+      }),
+    })
+
+    console.log("postsBusId", postsBusId)
+
     const homeRevalidate = await fetch("/api/revalidate?path=/")
-    const idRevalidate = await fetch(`/api/revalidate?path=/id/${slug}`)
+    // const idRevalidate = await fetch(`/api/revalidate?path=/id/${slug}`) // should be alias
 
     // const dispatchEmails = await fetch("/api/email-dispatch", {
     //   method: "POST",
@@ -219,12 +238,12 @@ export const NewPost = ({ slug }: { slug: string }) => {
     //   })
     // })
 
-    // location.reload()
+    location.reload()
     // redirect(`/orgs/${params.slug}`)
   }
 
   return (
-    <div className="border-primary mb-6 flex flex-col gap-2 max-w-xl p-3 drop-shadow-sm">
+    <div className="border-primary mb-6 flex flex-col gap-2 max-w-xl drop-shadow-sm">
       {imageSrcs[0] && (
         <div className="flex grow items-center justify-center">
           <img
@@ -244,7 +263,7 @@ export const NewPost = ({ slug }: { slug: string }) => {
             <IconCalendarEvent size={16} /> Event
           </PostTypeButton>
           {(title || type === "event") && (
-            <Input
+            <SCNInput
               type="text"
               id="title"
               placeholder="Title"
@@ -258,7 +277,7 @@ export const NewPost = ({ slug }: { slug: string }) => {
 
         <div className="flex flex-col gap-2">
           {type === "event" && (
-            <Input
+            <SCNInput
               type="text"
               id="location"
               placeholder="Location"
@@ -271,29 +290,42 @@ export const NewPost = ({ slug }: { slug: string }) => {
           <div className="flex flex-wrap items-center gap-2">
             {type === "event" && (
               <>
-                <input
+                <DatePicker startDate={startDate} setStartDate={setStartDate} />
+                {/* <input
                   type="date"
                   value={startDate}
                   onChange={e =>
                     setStartDate(toValidDateString(new Date(e.target.value)))
                   }
                   className="border-primary focus:outline-primary border bg-white px-1 font-medium text-[#8258ff] text-opacity-50 focus:outline-dashed focus:outline-1"
-                />
+                /> */}
                 {!allDay && (
-                  <input
-                    type="time"
-                    value={startTime}
-                    onChange={e => {
-                      // console.log("e", e)
-                      setStartTime(e.currentTarget.value)
-                    }}
-                    step="300"
-                    className="border-primary focus:outline-primary border bg-white px-1 font-medium text-[#8258ff] text-opacity-50 focus:outline-dashed focus:outline-1"
-                  />
+                  <TimePicker />
+                  // <SCNInput
+                  //   type="time"
+                  //   value={`${startDate?.getHours() || "00"}:${
+                  //     startDate?.getMinutes() || "00"
+                  //   }`}
+                  //   onChange={e => {
+                  //     const [hours, minutes] = e.currentTarget.value
+                  //       .split(":")
+                  //       .map(Number)
+
+                  //     setStartDate(startDate => {
+                  //       const newStartDate = new Date(startDate as Date)
+                  //       startDate?.setHours(hours)
+                  //       startDate?.setMinutes(minutes)
+                  //       startDate?.setSeconds(0)
+                  //       startDate?.setMilliseconds(0)
+                  //       return newStartDate
+                  //     })
+                  //   }}
+                  //   step="300"
+                  //   className="w-32 border-primary focus:outline-primary border bg-white font-medium text-[#8258ff] text-opacity-50 focus:outline-dashed focus:outline-1"
+                  // />
                 )}
 
-                <label className="flex items-center gap-2 text-xs uppercase opacity-80">
-                  All Day?
+                <label className="flex items-center gap-1 ml-2 text-xs uppercase opacity-80">
                   <input
                     type="checkbox"
                     id="allday"
@@ -302,6 +334,7 @@ export const NewPost = ({ slug }: { slug: string }) => {
                     onChange={e => setAllDay(e.target.checked)}
                     className="outline-primary checked:bg-primary mr-1 outline-4"
                   />
+                  All Day
                 </label>
               </>
             )}
@@ -322,6 +355,111 @@ export const NewPost = ({ slug }: { slug: string }) => {
         </div>
       </form>
     </div>
+  )
+}
+
+const HTMLElementWrapper = ({ element }: { element: HTMLElement | null }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  // Check if element is valid
+  if (!element) {
+    return null // Return null if element is not valid
+  }
+
+  // Append the element to the wrapper div
+  if (wrapperRef.current) {
+    wrapperRef.current.appendChild(element)
+  }
+
+  // Return the wrapper div's DOM node
+  return <div ref={wrapperRef} />
+}
+
+function TimePicker() {
+  const containerRef = useRef(null)
+
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorTextPlaceholder: "#8258ff80",
+          controlItemBgActive: "rgb(221 210 255 / 1)",
+        },
+        components: {
+          DatePicker: {
+            hoverBorderColor: "#000",
+            activeBorderColor: "#000",
+            cellActiveWithRangeBg: "rgb(221 210 255 / 0.5)",
+          },
+        },
+      }}>
+      <div
+        ref={containerRef}
+        style={{ position: "relative" }}
+        className="my-time-picker"
+      />
+      <AntDTimePicker
+        use12Hours
+        suffixIcon={
+          <IconClock size={16} className="text-primarydark text-opacity-50" />
+        }
+        clearIcon={
+          <IconX size={16} className="text-primarydark text-opacity-50" />
+        }
+        removeIcon={
+          <IconX size={16} className="text-primarydark text-opacity-50" />
+        }
+        format="h:mm a"
+        getPopupContainer={() => {
+          if (!containerRef.current) return document.body
+          console.log("containerRef", containerRef.current)
+          return containerRef.current
+        }}
+        minuteStep={5}
+        needConfirm={false}
+        className="rounded-none my-time-picker border-primary hover:border-primary font-sans"
+        popupStyle={{
+          boxShadow: "none",
+          border: "1px solid rgb(221 210 255 / 1)",
+          borderRadius: "0",
+          zIndex: 1070,
+          backgroundColor: "white",
+        }}
+        // popupClassName="*:rounded-none border-primary hover:border-primary font-sans"
+      />
+    </ConfigProvider>
+  )
+}
+
+function TimePickerr() {
+  return (
+    <Select>
+      <SelectTrigger className="w-28 rounded-none border-primary py-0 placeholder:text-[#8258ff] placeholder:text-opacity-50">
+        <SelectValue placeholder="Time?" />
+      </SelectTrigger>
+      <SelectContent>
+        {Array.from({ length: 24 }).map((_, hourIndex) => (
+          <>
+            <SelectItem value={`${hourIndex}:00`} key={`${hourIndex}:00`}>
+              {hourIndex % 12 === 0 ? 12 : hourIndex % 12}:00
+              {hourIndex < 12 ? "am" : "pm"}
+            </SelectItem>
+            <SelectItem value={`${hourIndex}:15`} key={`${hourIndex}:15`}>
+              {hourIndex % 12 === 0 ? 12 : hourIndex % 12}:15
+              {hourIndex < 12 ? "am" : "pm"}
+            </SelectItem>
+            <SelectItem value={`${hourIndex}:30`} key={`${hourIndex}:30`}>
+              {hourIndex % 12 === 0 ? 12 : hourIndex % 12}:30
+              {hourIndex < 12 ? "am" : "pm"}
+            </SelectItem>
+            <SelectItem value={`${hourIndex}:45`} key={`${hourIndex}:45`}>
+              {hourIndex % 12 === 0 ? 12 : hourIndex % 12}:45
+              {hourIndex < 12 ? "am" : "pm"}
+            </SelectItem>
+          </>
+        ))}
+      </SelectContent>
+    </Select>
   )
 }
 
@@ -349,72 +487,6 @@ export function Description(props: {
               : ""
           )
         }
-        className="border-primary focus:outline-primary w-full border bg-white p-1 text-base placeholder:text-[#8258ff] placeholder:opacity-40 focus:outline-dashed focus:outline-1"
-      />
-    </div>
-  )
-}
-
-export function UploadImageButton({
-  imageSrcs,
-  setImageSrcs,
-}: {
-  imageSrcs: string | string[]
-  setImageSrcs:
-    | Dispatch<SetStateAction<string[]>>
-    | Dispatch<SetStateAction<string>>
-}) {
-  const [file, setFile] = useState<File | null>(null)
-  const [loading, setLoading] = useState(false)
-  const client = useClient()
-  // console.log("file", file)
-
-  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0])
-      setLoading(true)
-    }
-  }
-
-  useEffect(() => {
-    try {
-      file &&
-        client?.uploadFile(file).then((result: any) => {
-          console.log("result", result)
-          if (typeof imageSrcs === "string") {
-            const newImageSrc = getMxcUrl(result.content_uri)
-            console.log("newImageSrc", newImageSrc)
-            ;(setImageSrcs as Dispatch<SetStateAction<string>>)(newImageSrc)
-          } else {
-            const newImageSrcs = [...imageSrcs, getMxcUrl(result.content_uri)]
-            console.log("newImageSrcs", newImageSrcs)
-            ;(setImageSrcs as Dispatch<SetStateAction<string[]>>)(newImageSrcs)
-          }
-          setFile(null)
-          setLoading(false)
-        })
-      // if (!result) return
-    } catch (error) {
-      console.error("error", error)
-    }
-  }, [file, imageSrcs, setImageSrcs, client])
-
-  return (
-    <div className="">
-      <label
-        htmlFor="image"
-        className="border-primary flex cursor-pointer items-center justify-center rounded-[100%] border border-dashed px-2 py-1 text-[#9572ff]">
-        {loading ? (
-          <Spinner className="text-primary h-4 w-4 animate-spin fill-[#9572ff]" />
-        ) : (
-          <IconCamera size={16} />
-        )}
-      </label>
-      <input
-        type="file"
-        onChange={handleFileChange}
-        id="image"
-        className="hidden"
       />
     </div>
   )
