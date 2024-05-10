71c707076b56f180385214b61d8568c3da02bedb
Meri Leeworthy <mlee0167@student.monash.edu>
2024-05-10 12:42:10 +1000

    debug

--- src/app/post/[id]/Form.tsx
+++ src/app/post/[id]/Form.tsx
@@ -3,19 +3,27 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { addTagToPost } from "../actions"
-import { useClient } from "@/hooks/useClient"
+// import { useClient } from "@/hooks/useClient"
+
+// ideally this should search for tags as you type, then you can click ones that come up or choose to add a new one
 
 export function Form({ postId }: { postId: string }) {
   return (
     <form
       action={async (formData: FormData) => {
-        const res = await addTagToPost(formData)
-        console.log("res", res)
+        console.log("formData tag", formData.get("tag"))
+        console.log("formData postId", formData.get("postId"))
+        try {
+          const res = await addTagToPost(formData)
+          console.log("res", res)
+        } catch (e) {
+          console.error(e)
+        }
       }}
       className="flex flex-col gap-2 p-2 max-w-sm">
       <input type="hidden" name="postId" value={postId} />
       <Input type="text" name="tag" />
-      <Button type="submit">Add Tag</Button>
+      <Button>Add Tag</Button>
     </form>
   )
 }
