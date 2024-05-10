cd13228d1a95b3e96dc73ec6339279161ed5e4f6
Meri Leeworthy <mlee0167@student.monash.edu>
2024-05-10 12:19:28 +1000

    section/article, not list

--- src/components/ui/FlexGridList.tsx
+++ src/components/ui/FlexGridList.tsx
@@ -1,25 +1,27 @@
 export function FlexGridList({ children }: { children: React.ReactNode }) {
-  return <ul className="flex flex-wrap gap-2">{children}</ul>
+  return <section className="flex flex-wrap gap-2">{children}</section>
 }
 
 export function FlexGridListItem({ children }: { children: React.ReactNode }) {
   return (
-    <li className="my-4 w-full pt-1 sm:w-80 relative group transition list-none">
+    <article className="my-4 w-full pt-1 sm:w-80 relative group transition list-none">
       <div className="absolute inset-0 border-t border-green-400 transition group-hover:border-t-4"></div>
       <div className="relative z-10">{children}</div>
-    </li>
+    </article>
   )
 }
 
 export function FlexList({ children }: { children: React.ReactNode }) {
-  return <ul className="flex flex-col items-start gap-6">{children}</ul>
+  return (
+    <section className="flex flex-col items-start gap-6">{children}</section>
+  )
 }
 
 export function FlexListItem({ children }: { children: React.ReactNode }) {
   return (
-    <li className="pl-3 w-full relative group transition max-w-xl">
+    <article className="pl-3 w-full relative group transition max-w-xl">
       <div className="absolute inset-0 border-l-2 border-green-400 transition group-hover:border-l-4"></div>
       <div className="relative z-10">{children}</div>
-    </li>
+    </article>
   )
 }
