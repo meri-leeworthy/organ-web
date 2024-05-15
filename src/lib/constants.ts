73f4d128b5350880479ab36a3605cf802ba3d6c4
vespyrs <desirepaths@proton.me>
2024-05-15 11:11:18 +1000

    hardcode url for upload image thumbnail

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const HOME_SPACE = "!LYcDqbaOzMrwVZsVRJ:radical.directory"
-// export const BASE_URL = "https://matrix.radical.directory" // use process.env.MATRIX_BASE_URL
+export const BASE_URL = "https://matrix.radical.directory" // use process.env.MATRIX_BASE_URL
 export const ACCESSTOKEN_STORAGE_KEY = "accessToken"
 export const USERID_STORAGE_KEY = "userId"
